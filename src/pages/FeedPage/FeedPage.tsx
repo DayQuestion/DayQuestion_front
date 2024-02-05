63587709ee6bc6f625882c89e84f5bf431a5ebf7
박현민 <dev.hmpark@gmail.com>
2024-02-05 19:13:07 +0900

    feat: UserComponent  추가 및 Feed Page 개발

--- src/pages/FeedPage/FeedPage.tsx
+++ src/pages/FeedPage/FeedPage.tsx
@@ -1,38 +1,35 @@
 import React from "react";
 import styles from "./FeedPage.module.css";
-import Nav from "../../components/common/Nav";
-import Search from "../../components/common/Search";
+import { Nav_hm } from "../../components/common/Nav_hm";
+import SearchHm from "../../components/common/Search_hm";
 import QuestionContainer from "../../components/common/QuestionContainer";
 import AnswerContainer from "../../components/common/AnswerContainer";
+import UserContainer from "../../components/common/UserContainer";
 
 function FeedPage() {
   // 현재 날짜를 가져옵니다.
   const today = new Date();
 
   // 원하는 형식으로 날짜를 설정합니다.
-  const formattedDate = `< ${today.getFullYear()}년 ${today.getMonth() + 1}월 ${today.getDate()}일 >`;
+  const formattedDate = `< ${today.getFullYear()}. ${today.getMonth() + 1}. ${today.getDate()} >`;
 
   return (
     <section className={styles.feed}>
       <div className={styles.container}>
         <div className={styles.left}>
-          <Search />
-          <div className={styles.leftUsers}>
-            <h1>User</h1>
-          </div>
-          <div className={styles.leftFooter}>
-            <Nav />
-          </div>
+          <SearchHm />
+          <UserContainer />
+          <Nav_hm />
         </div>
         <div className={styles.right}>
           <div className={styles.rightHeader}>
-            <text>{formattedDate} </text>
+            <text className={styles.rightHeaderText}>{formattedDate} </text>
           </div>
-          <div>
+          <div className={styles.rightContent}>
             <QuestionContainer />
+            <AnswerContainer />
+            <AnswerContainer />
           </div>
-          <AnswerContainer />
-          <AnswerContainer />
         </div>
       </div>
     </section>
