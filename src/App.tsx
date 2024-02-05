728e0d0ec8fa440cdb25dc993ad9de8b33f3fa67
O.O_ixn <gmltjr000@gmail.com>
2024-02-05 17:56:32 +0900

    myPage, QnAPage 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import FeedPage from "./pages/FeedPage/FeedPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import QnAPage from "./pages/QnAPage/QnAPage";
@@ -13,7 +13,7 @@ function App() {
     <Routes>
       <Route index element={<LoginPage />}></Route>
       <Route path="/signUp" element={<SignUpPage />}></Route>
-      <Route path="/profile" element={<MyPage />}></Route>
+      <Route path="/myPage" element={<MyPage />}></Route>
       <Route path="/qna" element={<QnAPage />}></Route>
       <Route path="/friends" element={<FriendsPage />}></Route>
       <Route path="/feed" element={<FeedPage />}></Route>
