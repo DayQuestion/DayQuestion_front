f9fcb39c1ea7df08d18bfdcfd143099287803ae8
O.O_ixn <gmltjr000@gmail.com>
2024-02-07 15:15:47 +0900

    날짜에 맞게 질문 생성

--- src/assets/data/data.ts
+++ src/assets/data/data.ts
@@ -1,72 +1,76 @@
-export const question = [
-    {
-      "question": "내 삶의 목적은 무엇인가?",
-      "translation": "What is your purpose in life?"
-    },
-    {
-      "question": "사람은 변할 수 있을까?",
-      "translation": "Can people change?"
-    },
-    {
-      "question": "현재 읽고 있는 글이나 책이 있다면?",
-      "translation": "What are you reading right now?"
-    },
-    {
-      "question": "내 삶에서 가장 버리고 싶은 것은?",
-      "translation": "What is something you want to throw out in your life?"
-    },
-    {
-      "question": "가장 최근에 방문한 식당은? 무엇을 먹었는가?",
-      "translation": "What was the last restaurant you went to? What did you have there?"
-    },
-    {
-      "question": "오늘 하루가 힘들었던 이유는 ..... (이)다.",
-      "translation": "Today was tough because ......"
-    },
-    {
-      "question": "나는 운이 좋은 편이라고 생각하는가?",
-      "translation": "Do you think you're lucky? How so?"
-    },
-    {
-      "question": "계속 머릿속에 맴도는 노래가 있다면?",
-      "translation": "What song is stuck in your head?"
-    },
-    {
-      "question": "오늘은 평범한 하루였는가? 그렇거나 그렇지 않은 이유는?",
-      "translation": "Was today typical? Why or why not?"
-    },
-    {
-      "question": "내 인생을 바꿀만한 영감을 준 사람이 있다면?",
-      "translation": "Is there anyone who gave you the life-changing inspiration?"
-    }
+interface QuestionData {
+  question: string;
+  question_en: string;
+}
 
-  ]
-  
+export const questions: { [key: string]: QuestionData } = {
+  "2024-02-07": {
+    question: "내 삶의 목적은 무엇인가?",
+    question_en: "What is your purpose in life?",
+  },
+  "2024-02-08": {
+    question: "사람은 변할 수 있을까?",
+    question_en: "Can people change?",
+  },
+  "2024-02-09": {
+    question: "현재 읽고 있는 글이나 책이 있다면?",
+    question_en: "What are you reading right now?",
+  },
+  "2024-02-10": {
+    question: "내 삶에서 가장 버리고 싶은 것은?",
+    question_en: "What is something you want to throw out in your life?",
+  },
+  "2024-02-11": {
+    question: "가장 최근에 방문한 식당은? 무엇을 먹었는가?",
+    question_en:
+      "What was the last restaurant you went to? What did you have there?",
+  },
+  "2024-02-12": {
+    question: "오늘 하루가 힘들었던 이유는 ..... (이)다.",
+    question_en: "Today was tough because ......",
+  },
+  "2024-02-13": {
+    question: "나는 운이 좋은 편이라고 생각하는가?",
+    question_en: "Do you think you're lucky? How so?",
+  },
+  "2024-02-14": {
+    question: "계속 머릿속에 맴도는 노래가 있다면?",
+    question_en: "What song is stuck in your head?",
+  },
+  "2024-02-15": {
+    question: "오늘은 평범한 하루였는가? 그렇거나 그렇지 않은 이유는?",
+    question_en: "Was today typical? Why or why not?",
+  },
+  "2024-02-16": {
+    question: "내 인생을 바꿀만한 영감을 준 사람이 있다면?",
+    question_en: "Is there anyone who gave you the life-changing inspiration?",
+  },
+};
+
 export const user = [
-    {
-      "id": 1,
-      "name": "김희석",
-      "email": "ooixn@example.com",
-    },
-    {
-      "id": 2,
-      "name": "김예지",
-      "email": "kimpiczza@example.com",
-    },
-    {
-      "id": 3,
-      "name": "김민희",
-      "email": "minnie5382@example.com",
-    },
-    {
-      "id": 4,
-      "name": "정시온",
-      "email": "siontext@example.com",
-    },
-    {
-      "id": 5,
-      "name": "박현민",
-      "email": "dev.hmpark@example.com",
-    }
-  ]
-  
+  {
+    id: 1,
+    name: "김희석",
+    email: "ooixn@example.com",
+  },
+  {
+    id: 2,
+    name: "김예지",
+    email: "kimpiczza@example.com",
+  },
+  {
+    id: 3,
+    name: "김민희",
+    email: "minnie5382@example.com",
+  },
+  {
+    id: 4,
+    name: "정시온",
+    email: "siontext@example.com",
+  },
+  {
+    id: 5,
+    name: "박현민",
+    email: "dev.hmpark@example.com",
+  },
+];
